eeb7d8fcdfc81a12fd5b831e4531c7716dcb24bf
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2024-03-04 15:09:36 +0100

    normalizados y representación básica de los datos

--- analisis.py
+++ analisis.py
@@ -6,7 +6,11 @@ from opensignalsreader import OpenSignalsReader
 data = OpenSignalsReader('data_diego_txt/EDA.txt')
 data2 = OpenSignalsReader('data_diego_txt/EMG.txt')
 data3 = OpenSignalsReader('data_diego_txt/LUX_ACC.txt')
-
+data4 = OpenSignalsReader('data_javi_txt/ACC_J.txt')
+data5 = OpenSignalsReader('data_javi_txt/ECG_J.txt')
+data6 = OpenSignalsReader('data_javi_txt/EDA_J.txt')
+data7 = OpenSignalsReader('data_javi_txt/EMG_J.txt')
+data8 = OpenSignalsReader('data_javi_txt/LUX_J.txt')
 # Ploteamos las señales usando matplotlib
 
 # Señal de EDA (Electrodermal Activity)
@@ -40,3 +44,14 @@ plt.title('LUX signal')
 plt.xlabel('Sample')
 plt.ylabel('Value')
 plt.show()
+
+# Señal de Acelerómetro 2
+
+plt.plot(data4.raw([1]))
+plt.title('ACC signal')
+plt.xlabel('Sample')
+plt.ylabel('Value')
+plt.show()
+
+# Señal de LUX 2
+
